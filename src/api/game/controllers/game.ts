426120cb333cede641abe1c4d774843e1dd2114e
Rafael Ribeiro de Sousa <faelribeirodesousa@gmail.com>
2025-03-13 10:09:45 -0300

    create new api

--- src/api/game/controllers/game.ts
+++ src/api/game/controllers/game.ts
@@ -4,4 +4,10 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::game.game');
+export default factories.createCoreController('api::game.game', ({ strapi }) => ({
+  async populate(ctx) {
+    console.log('Rodando');
+
+    ctx.send('finish')
+  }
+}));
